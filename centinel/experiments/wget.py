39ea48d8b88f87e2be9dbbeb74fbc4460daaf36c
Naman Mittal <namanmittal003@gmail.com>
2015-04-13 02:50:41 -0400

    wget experiment completed with comments

--- centinel/experiments/wget.py
+++ centinel/experiments/wget.py
@@ -2,10 +2,13 @@ import os
 import logging
 import time
 from centinel.experiment import Experiment
+''' This experiment is beign written to detect censorship on the basis of length if a web-page. 
+In this experiment we ran wget command to download and save a web page and,
+then check file stat to get the size of downloaded page and append into the result for evaluation.'''
 
 class CensorShipExperiment(Experiment):
-    name = "wget"
-
+    name = "wget" #Name of the experiment
+	
     def __init__(self, input_file):
         self.input_file  = input_file
         self.results = []
@@ -17,21 +20,18 @@ class CensorShipExperiment(Experiment):
 
     def wget_test(self):
         result = {
-            "host" : self.host,
+            "host" : self.host,	#First field in JSON object 
         }
 
         logging.info("Running wget to %s" % self.host)
-        response = os.system("wget -O " + self.host+ " "+self.host)
-	#os.system("ls -l "+self.host+" > file_stat.txt")
-	size_info = os.stat(self.host)
+        response = os.system("wget -O " + self.host+ " "+self.host) #Run wget command to download the index.html page of the request host and save that page as the same name of the host.
+	size_info = os.stat(self.host) #Get the size of the webpage saved
 	size = size_info.st_size
 	print size
-        if size != 0:
+        if size != 0 && response == 0: # we will get response equal to 0 only when wget dont get any errors
             result["success"] = 'true'
 	    result["size"] = size
         else:
             result["success"] = 'false'
-	    result["size"] = size	
+	    result["size"] = 0
         self.results.append(result)
-#	time.sleep(20)
-	print response
